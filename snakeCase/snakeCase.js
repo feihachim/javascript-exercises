e0916413b33052fadc5c3da0541d45988d6db167
Hachim Youssoufa <feihachim@yahoo.fr>
2021-09-05 13:00:32 +0200

    snakeCase exercise done

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,17 @@
-const snakeCase = function() {
-
+const snakeCase = function (str) {
+    let result;
+    let lowerArrStr;
+    let arrStr = str.match(/\w+/g);
+    if (arrStr.length > 1) {
+        lowerArrStr = arrStr.map(element => element.toLowerCase());
+    }
+    else {
+        let indexUpper = arrStr[0].search(/[A-Z]/);
+        let arrResult = [arrStr[0].substr(0, indexUpper), arrStr[0].substr(indexUpper)];
+        lowerArrStr = arrResult.map(element => element.toLowerCase());
+    }
+    result = lowerArrStr.join('_');
+    return result;
 };
 
 module.exports = snakeCase;
